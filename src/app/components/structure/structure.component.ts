62d93dc5edb1ef94bf3656d322c147154469c9ac
Harish Agrawal <harish.agrawal@pegasusinfocorp.com>
2019-10-14 14:10:34 +0530

    Structure Design and Functionality completed

--- src/app/components/structure/structure.component.ts
+++ src/app/components/structure/structure.component.ts
@@ -1,15 +1,191 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {AuthService} from '../../shared/auth-service.service';
+import {CommonService} from '../../shared/common.service';
+import {HttpApiService} from '../../shared/http-api.service';
+import {NgxSpinnerService} from 'ngx-spinner';
 
 @Component({
   selector: 'app-structure',
   templateUrl: './structure.component.html',
   styleUrls: ['./structure.component.styl']
 })
-export class StructureComponent implements OnInit {
+export class StructureComponent implements OnInit, AfterViewInit {
+  @Output() nextStep: EventEmitter<string> = new EventEmitter<string>();
+  round = new FormControl('');
+  leftSelect = [];
+  rightSelect = [];
 
-  constructor() { }
+  leftColumn = [
+    {name: 'Identifier', checked: false, value: 1},
+    {name: 'Team Name Entry', checked: false, value: 1},
+    {name: 'Info (Case Study/Scenario )', checked: false, value: 1},
+    {name: 'Image ', checked: false, value: 1},
+    {name: 'Carousel ', checked: false, value: 1},
+    {name: 'Team Info ', checked: false, value: 1},
+    {name: 'Idea hunt ', checked: false, value: 1},
+    {name: 'Brainstorm ', checked: false, value: 1},
+    {name: 'Custom Activity ', checked: false, value: 1},
+    {name: 'Leader Board ', checked: false, value: 1},
+  ];
+
+  rightColumn = [
+    {name: 'Single Select', checked: false, value: 1, feedback: false, result: false},
+    {name: 'Multiple Select', checked: false, value: 1, feedback: false, result: false},
+    {name: 'Slider', checked: false, value: 1, feedback: false, result: false},
+    {name: 'Drag & Drop', checked: false, value: 1, feedback: false, result: false},
+    {name: 'Dropdown', checked: false, value: 1, feedback: false, result: false},
+    {name: 'Scale-Rating', checked: false, value: 1, feedback: false, result: false},
+    {name: 'Toggle', checked: false, value: 1, feedback: false, result: false}
+  ];
+
+  constructor(private authService: AuthService, private commonService: CommonService,
+              private http: HttpApiService, private spinner: NgxSpinnerService,
+              private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    const setInt = setInterval(() => {
+      const contentData: any = this.commonService.contentObject;
+      if (contentData && contentData.content && contentData.content.structure) {
+        this.round.setValue(contentData.content.structure.round);
+        this.leftSelect = contentData.content.structure.leftSelect;
+        this.rightSelect = contentData.content.structure.rightSelect;
+
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0; i < this.leftSelect.length; i ++) {
+          const checked = this.leftSelect.map((o) => { return o.name; }).indexOf(this.leftSelect[i].name);
+          if (!(checked === -1)) {
+            this.leftColumn[checked].checked = true;
+            this.leftColumn[checked].value = this.leftSelect[i].value;
+          }
+        }
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0; i < this.rightSelect.length; i ++) {
+          const checked = this.rightColumn.map((o) => { return o.name; }).indexOf(this.rightSelect[i].name);
+          if (!(checked === -1)) {
+            this.rightColumn[checked].checked = true;
+            this.rightColumn[checked].value = this.rightSelect[i].value;
+            this.rightColumn[checked].feedback = this.rightSelect[i].feedback;
+            this.rightColumn[checked].result = this.rightSelect[i].result;
+          }
+        }
+        clearInterval(setInt);
+        this.cdr.detectChanges();
+      }
+    }, 500);
+  }
+
+  leftColumnTab(left, i) {
+    const index = this.leftSelect.indexOf(left);
+    if (index === -1) {
+      this.leftColumn[i].checked = true;
+      this.leftSelect.push(left);
+    } else {
+      this.leftColumn[i].checked = false;
+      this.leftColumn[i].value = 1;
+      this.leftSelect.splice(index, 1);
+    }
+  }
+
+
+  rightColumnTab(right, i) {
+    const index = this.rightSelect.indexOf(right);
+    if (index === -1) {
+      this.rightColumn[i].checked = true;
+      this.rightSelect.push(right);
+    } else {
+      this.rightColumn[i].checked = false;
+      this.rightColumn[i].value = 1;
+      this.rightSelect.splice(index, 1);
+    }
+  }
+
+  onChange(obj) {
+    const checked = this.rightSelect.map((o) => { return o.name; }).indexOf(obj.name);
+    this.rightSelect[checked].feedback = obj.feedback;
+    this.rightSelect[checked].result = obj.result;
+  }
+
+  onChangeNumber(obj, e, side) {
+    if (side === 'right') {
+      const checked = this.rightSelect.map((o) => { return o.name; }).indexOf(obj.name);
+      this.rightSelect[checked].value = e.target.value;
+    } else {
+      const checked = this.leftSelect.map((o) => { return o.name; }).indexOf(obj.name);
+      this.leftSelect[checked].value = e.target.value;
+    }
+  }
+
+  save() {
+    if (this.round.value === '') {
+      this.authService.openSnackBar('Please enter number of round', false);
+      return false;
+    }
+
+    const contentData: any = this.commonService.contentObject;
+    let basicInfo = {};
+    if (contentData.content.basicInfo) {
+      basicInfo = contentData.content.basicInfo;
+    }
+    let hostingStrategy = {};
+    if (contentData.content && contentData.content.hostingStrategy !== undefined) {
+      hostingStrategy = contentData.content.hostingStrategy;
+    }
+    let design = {};
+    if (contentData.content && contentData.content.design !== undefined) {
+      design = contentData.content.design;
+    }
+    let content = {};
+    if (contentData.content && contentData.content.content !== undefined) {
+      content = contentData.content.content;
+    }
+    let score = {};
+    if (contentData.content && contentData.content.score !== undefined) {
+      score = contentData.content.score;
+    }
+    let participant = {};
+    if (contentData.content && contentData.content.participant !== undefined) {
+      participant = contentData.content.participant;
+    }
+
+    const structure = {
+      round: this.round.value,
+      rightSelect: this.rightSelect,
+      leftSelect: this.leftSelect
+    };
+    const formdata = {
+      createRequestID: localStorage.getItem('_id'),
+      content: {
+        basicInfo,
+        hostingStrategy,
+        participant,
+        design,
+        content,
+        structure,
+        score
+      }
+    };
+
+    this.spinner.show();
+    this.http.postApi('content', formdata).subscribe(res => {
+      const result: any = res;
+      if (result.success) {
+        this.authService.openSnackBar('Successfully added', true);
+        this.http.getApi('content/' + localStorage.getItem('_id')).subscribe(res1 => {
+          const ret: any = res1;
+          this.commonService.contentObject = ret.message;
+          this.nextStepFun();
+        });
+      } else {
+        this.authService.openSnackBar('Please try again', false);
+      }
+    });
+  }
+
+  nextStepFun() {
+    this.nextStep.emit('6');
+  }
 }
